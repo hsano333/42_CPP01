ede7e032f04d9597e9d541caf950ccd33ba2cc37
Hiroaki Sano <hsano@student.42tokyo.jp>
2023-03-06 20:14:40 +0900

    fixed ex01

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -2,23 +2,47 @@
 #include <string>
 #include <iostream>
 
+/*
+#include <stdio.h>
+__attribute__((destructor)) void f(void){
+    system("leaks Moar_brainz!");
+}
+*/
+
 int main(void)
 {
-    int N = 5;
-    Zombie* zombies;
-
-	Zombie zombie("static");
-	zombies = zombie.zombieHorde(N, "Horde");
-
-    for (int i=0; i<N;i++)
     {
-        std::cout << "address:" << &(zombies[i]) << " announce i=" << i << std::endl;
-        zombies[i].announce();
-    }
+        int N = 5;
+        Zombie* zombies;
 
-	std::cout << "destroy " << std::endl;
-	std::cout << "end main" << std::endl;
-    delete [] zombies;
-    //system("leaks Moar_brainz!");
+        Zombie zombie("static");
+        zombies = zombie.zombieHorde(N, "Horde");
+
+        for (int i=0; i<N;i++)
+        {
+            std::cout << "address:" << &(zombies[i]) << " announce i=" << i << std::endl;
+            zombies[i].announce();
+        }
+
+        std::cout << "destroy " << std::endl;
+        std::cout << "end main" << std::endl;
+        delete [] zombies;
+    }
+    {
+        int N = -1;
+        Zombie* zombies;
+
+        Zombie zombie("static");
+        zombies = zombie.zombieHorde(N, "Horde");
+
+        for (int i=0; i<N;i++)
+        {
+            std::cout << "address:" << &(zombies[i]) << " announce i=" << i << std::endl;
+            zombies[i].announce();
+        }
+
+        std::cout << "destroy " << std::endl;
+        std::cout << "end main" << std::endl;
+    }
 	return (0);
 }
--- ex01/srcs/zombieHorde.cpp
+++ ex01/srcs/zombieHorde.cpp
@@ -4,6 +4,8 @@
 
 Zombie* Zombie::zombieHorde(int N, std::string name)
 {
+    if (N <= 0)
+        return NULL;
     Zombie* zombies = new Zombie[N];
     //std::cout << "allocated address:" << &(zombies[0]) << std::endl;
     for (int i = 0; i < N; i++)
